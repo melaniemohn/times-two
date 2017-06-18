b0e9a3f6f77dbbebc68c5edb60ae9d1eeb5716a4
Melanie Mohn <melaniemohn@yahoo.com>
2017-06-18 01:49:40 -0400

    capturing guess data yay

--- scripts/content.js
+++ scripts/content.js
@@ -1,24 +1,14 @@
 
-// and for highlight, listen for click OR an arrow key???
-
 console.log('inside the content script')
 
 
-// to change color of own active square
-
-var guessIndex = $('.guess').index()
-var active = $('.active').index('.flex-cell');
-
-var highlights = $('.highlight').each(function(square){
-	console.log('each square', square)
-})
-
 // helper / utility functions
 // getLetter and/or putLetter???
+
 // use these to communicate with background script??
 
-
-function highlight(event){
+// define separate functions for myHighlight and otherHighlight?
+function highlight(event) { // to change color of own active square
 	console.log('event: ', event)
 		$('.flex-cell').not('.black').css('background-color', 'white')
 		$('.highlighted').css('background-color', '#a7d8ff')
@@ -26,58 +16,43 @@ function highlight(event){
 		console.log('cell index: ', $('.active').index('.flex-cell'))
 }
 
+// for otherHighlight, also need to get that info with a timeout
 
-// write an event emitter that uses jQuery to track changes in the DOM
-// use a utility function from jQuery (index!!) to figure out WHICH of the huge # of squares is updating
 $(document).ready(function() {
-	console.log('ready or not (no, ready)')
 
-	// okay, changing the color of flex-cell works, inside OR outside .ready
-	// $('.flex-cell').css('background-color', '#3abfff')
-
-
-	// okay, before doing this, figure out when a cell becomes active
-	// $('.active').css('background-color', '#3abfff')
-	// could maybe just connect this to a click event??
-	// but, gonna have to solve the problem for other highlight, anyway
-	$('.active').trigger('makeActive');
-
-	// okay, this works for letters
-	$('.guess').on('DOMSubtreeModified',function(){
-		console.log('DOM modified!')
-		// here, need to capture the actual data and send it to background in a message
-		// use $(this) to determine which element triggers event?
-		// use .html()?
+	// here, capture the actual data and send it to background in a message
+	// get the cell ID, plus the actual letter that was guessed
+	// to get index, use .parent(), which only traverses up one level (as opposed to parents())
+	$('.guess').on('DOMSubtreeModified',function() {
+		console.log('guess', $(this).html())
+		console.log('index', $(this).parent().index('.flex-cell'))
 	});
 
+	// might be cleaner to exclude black cells here, but otoh, it's lovely to be able to use the square grid for dimensions / count
 	$('.flex-cell').each(function(square) {
 		console.log('each square', square);
+		// maybe assign IDs in here??
 	});
 
 
 	// $('.active').index('.flex-cell');
 
-
-	$('.flex-cell').not('.black').on('click', function(event){
-		setTimeout(highlight, 5, event)
+	// I hate this setTimeout, because it's clunky
+	// but I hate on 'click' because it doesn't actually track the active cell as it changes
+		// (in response to an arrow key, say, but also WHEN SOMEONE IS TYPING)
+	$('.flex-cell').on('click', function(event) {
+		setTimeout(highlight, 5, event);
 	});
 
-
-	// console.log('event: ', event)
-	// 	$('.flex-cell').not('.black').css('background-color', 'white')
-	// 	$('.highlighted').css('background-color', '#a7d8ff')
-	// 	$(this).css('background-color', '#3abfff')
-	// 	console.log('cell index: ', $(this).index('.flex-cell'))
-
-	$('.flex-cell').triggerHandler('click');
-
-
-	// $('div').each(function () {
-	//     var color = $(this).css("color");
-	//     if (color == "#FFDA00") {
-	//         $(this).css("color", '#a7d8ff');
-	//     }
+	// okay the keypress business here is just NOT working
+	// $('.flex-cell').on('keypress', function(event) {
+	// 	setTimeout(highlight, 5, event);
 	// });
+
+	// abstract these into a more general event??
+	$('.flex-cell').triggerHandler('click');
+	// $('.flex-cell').triggerHandler('keypress');
+
 
 
 	$('.highlighted').each(function(square) {
