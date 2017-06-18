0b1f8434b6e14a5e03cd4d2f7ffc1affe3df09a2
Melanie Mohn <melaniemohn@yahoo.com>
2017-06-18 01:18:13 -0400

    duct taped the active / highlight stuff with a setTimeout

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # times-two
 for stackathon
 
+# on Chrome extensions, generally
+content script is the JS we inject into the page, and it can actually touch the DOM.  The content script listens for changes in the DOM using jQuery.
+
+for adding / changing / removing letters, instead of listening for a keydown event, I'm just using DOMSubtreeModified
+
+then, the content script uses message passing to send this change to the background script, which emits a message to the back-end
+
+
 # putting some styling notes in here
 because whatever
 
@@ -16,8 +24,9 @@ don't worry about tracking their active cell
 light blue (active highlight cell)
 in their scss: .flex-cell.active background-color: #FFDA00
 but .flex-cell.highlighted.active ???
-    theirs - active background: FFDA00
-    	   - highlight background: A7D8FF
+    NYT current
+    - active background: FFDA00
+    - highlight background: A7D8FF
 
 brighter blues
 - 3abfff
@@ -31,5 +40,5 @@ lavender (other user's highlight)
 - e1dbec
 
 
-add some html using jQuery
+add some styling using jQuery
 font color="#9c27b0"
--- scripts/content.js
+++ scripts/content.js
@@ -1,69 +1,101 @@
-// content script is the JS we inject into the page, and it can actually touch the DOM
 
-// for letters, listen for a keydown event? or just DOMSubtreeModified (?)
 // and for highlight, listen for click OR an arrow key???
 
-console.log('lol content')
+console.log('inside the content script')
 
-// okay, but for guess and square these aren't IDs....
-var guess = document.getElementById("guess");
-var square = document.getElementById("flex-cell");
-var highlight = document.getElementsByClassName("flex-cell.highlighted");
-var activeSquare = document.getElementsByClassName("flex-cell.highlighted.active"); // or just flex-cell.active ????
-// getElementsByClassName RETURNS AN ARRAY OF ELEMENTS
-// loop over squares and add listeners to them idk
-// NO JUST USE JQUERY lol
 
-// oh lol but we're using jQuery so it's like
-// $(".class-name").html(" ");
-	// so to change color... $(".highlighted")???
-// $('.flex-cell').keyup(function(event){
-//  // play with event
-//  // use $(this) to determine which element triggers this event
-// });
-var guessIndex = $(".guess").index()
-var active = $(".active").index(".flex-cell");
-var highlights = $(".highlight").each(function(square){
-	console.log(square)
+// to change color of own active square
+
+var guessIndex = $('.guess').index()
+var active = $('.active').index('.flex-cell');
+
+var highlights = $('.highlight').each(function(square){
+	console.log('each square', square)
 })
 
-
 // helper / utility functions
+// getLetter and/or putLetter???
 // use these to communicate with background script??
+
+
+function highlight(event){
+	console.log('event: ', event)
+		$('.flex-cell').not('.black').css('background-color', 'white')
+		$('.highlighted').css('background-color', '#a7d8ff')
+		$('.active').css('background-color', '#3abfff')
+		console.log('cell index: ', $('.active').index('.flex-cell'))
+}
 
 
 // write an event emitter that uses jQuery to track changes in the DOM
 // use a utility function from jQuery (index!!) to figure out WHICH of the huge # of squares is updating
 $(document).ready(function() {
 	console.log('ready or not (no, ready)')
-})
 
-	// listen for changes in DOM using jQuery
-	// use message passing to communicate this change with background.js
+	// okay, changing the color of flex-cell works, inside OR outside .ready
+	// $('.flex-cell').css('background-color', '#3abfff')
+
+
+	// okay, before doing this, figure out when a cell becomes active
+	// $('.active').css('background-color', '#3abfff')
+	// could maybe just connect this to a click event??
+	// but, gonna have to solve the problem for other highlight, anyway
+	$('.active').trigger('makeActive');
+
+	// okay, this works for letters
+	$('.guess').on('DOMSubtreeModified',function(){
+		console.log('DOM modified!')
+		// here, need to capture the actual data and send it to background in a message
+		// use $(this) to determine which element triggers event?
+		// use .html()?
+	});
+
+	$('.flex-cell').each(function(square) {
+		console.log('each square', square);
+	});
+
+
+	// $('.active').index('.flex-cell');
+
+
+	$('.flex-cell').not('.black').on('click', function(event){
+		setTimeout(highlight, 5, event)
+	});
+
+
+	// console.log('event: ', event)
+	// 	$('.flex-cell').not('.black').css('background-color', 'white')
+	// 	$('.highlighted').css('background-color', '#a7d8ff')
+	// 	$(this).css('background-color', '#3abfff')
+	// 	console.log('cell index: ', $(this).index('.flex-cell'))
+
+	$('.flex-cell').triggerHandler('click');
+
+
+	// $('div').each(function () {
+	//     var color = $(this).css("color");
+	//     if (color == "#FFDA00") {
+	//         $(this).css("color", '#a7d8ff');
+	//     }
+	// });
+
+
+	$('.highlighted').each(function(square) {
+		console.log('each highlight', square);
+	});
+
+});
+
+
+// use message passing to communicate DOM change with background.js
 	// chrome.runtime.sendMessage( /* ... */ )
+			// 	chrome.extension.sendMessage({greeting: 'hello'}, function(response) {
+			// 	  console.log(response.farewell);
+			// 	});
 
 
-	// receive messages from background using onMessage
+// receive messages from background using onMessage
 	// chrome.runtime.onMessage.addListener( /* ... */ )
 
 
-
-// utility functions?
-// function id(name) { return document.getElementById(name)}
-// function exists(val) { return (val == null || val == undefined || val == "") ? false : true }
-// here, write getLetter and/or putLetter???
-
-// function init() {
-// 	console.log('content script initialized')
-
-// 	// this is basically its last thing to call?
-// 	chrome.extension.sendMessage({greeting: 'hello'}, function(response) {
-// 	  console.log(response.farewell);
-// 	});
-
-// }
-
-// where does the the original typing get emitted from?
-// in www, it was a draw event emitted in whiteboard.js
-// so we did whiteboard.on('draw')...
 
